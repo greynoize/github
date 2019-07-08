b9a3b4d419fa2b708877c63e24b5225b2d1a90bc
tomfo <kirill.matrosov979@gmail.com>
2019-07-08 18:44:11 +0300

    Bugfix

--- app/src/main/java/com/strangelove/github/ui/main/MainFragmentPagerAdapter.kt
+++ app/src/main/java/com/strangelove/github/ui/main/MainFragmentPagerAdapter.kt
@@ -1,6 +1,7 @@
 package com.strangelove.github.ui.main
 
 import android.content.Context
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -9,13 +10,14 @@ import com.strangelove.github.ui.profile.ProfileFragment
 import com.strangelove.github.ui.repositories.repositories_list.RepositoriesFragment
 
 class MainFragmentPagerAdapter(fragmentManager: FragmentManager, private val context: Context): FragmentPagerAdapter(fragmentManager) {
-    override fun getItem(position: Int): Fragment {
-        return if (position == REPOSITORY_FRAGMENT_POSITION) {
-            RepositoriesFragment()
-        } else {
-            ProfileFragment()
-        }
+    private var listOfFragments = mutableListOf<Fragment>()
+
+    init {
+        listOfFragments.add(RepositoriesFragment())
+        listOfFragments.add(ProfileFragment())
     }
+
+    override fun getItem(position: Int) = listOfFragments[position]
 
     override fun getPageTitle(position: Int): CharSequence? {
         if (position == REPOSITORY_FRAGMENT_POSITION) {
@@ -27,11 +29,15 @@ class MainFragmentPagerAdapter(fragmentManager: FragmentManager, private val con
         return null
     }
 
-    override fun getCount() = COUNT_OF_FRAGMENTS
+    override fun getCount() = listOfFragments.size
 
     private companion object {
         private const val REPOSITORY_FRAGMENT_POSITION = 0
         private const val PROFILE_FRAGMENT_POSITION = 1
-        private const val COUNT_OF_FRAGMENTS = 2
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        listOfFragments[position] = super.instantiateItem(container, position) as Fragment
+        return listOfFragments[position]
     }
 }
--- app/src/main/java/com/strangelove/github/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/strangelove/github/ui/profile/ProfileFragment.kt
@@ -5,34 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.Observable
 import androidx.fragment.app.Fragment
 import com.strangelove.github.R
 import org.koin.android.architecture.ext.viewModel
 
 class ProfileFragment : Fragment() {
     private val profileViewModel: ProfileViewModel by viewModel()
-    private lateinit var onPropertyChangedCallback: Observable.OnPropertyChangedCallback
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: com.strangelove.github.databinding.ProfileLayoutBinding = DataBindingUtil.inflate(inflater, R.layout.profile_layout, container, false)
         binding.viewModel = profileViewModel
 
-        onPropertyChangedCallback = object : Observable.OnPropertyChangedCallback() {
-            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                when (propertyId) {
-
-                }
-            }
-        }
-
-        profileViewModel.addOnPropertyChangedCallback(onPropertyChangedCallback)
         profileViewModel.loadProfile()
         return binding.root
-    }
-
-    override fun onDestroy() {
-        profileViewModel.removeOnPropertyChangedCallback(onPropertyChangedCallback)
-        super.onDestroy()
     }
 }
